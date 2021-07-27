9722f1a5edd52ffc3dfedd72b3d890273fba9c72
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-07-27 19:54:55 -0300

    setting up navigation controller

--- Example/Example/Controller/TriggerButtonViewController.m
+++ Example/Example/Controller/TriggerButtonViewController.m
@@ -27,14 +27,31 @@
 }
 
 - (void)buttonAction {
-    UINavigationController *navigationController = UINavigationController.new;
+    UINavigationController *navigationController = [self configureNavigationController];
     ModalViewController *modalViewController = ModalViewController.new;
     navigationController.viewControllers = @[modalViewController];
     navigationController.modalPresentationStyle = UIModalPresentationCustom;
+    // Configure navigationItems
+    [self configureNavigationItems:modalViewController];
+    modalViewController.navigationItem.title = @"Title";
     [self presentViewController:navigationController animated:YES completion:nil];
     
 }
 
+- (void)configureNavigationItems:(UIViewController*)controller {
+    controller.navigationItem.title = @"Title";
+    controller.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Left Button" style:UIBarButtonItemStylePlain target:self action:nil];
+    controller.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Right Button" style:UIBarButtonItemStylePlain target:self action:nil];
+}
 
+- (UINavigationController*)configureNavigationController {
+    // Configuring Navigation Bar
+    UINavigationController *navigationController = UINavigationController.new;
+    navigationController.navigationBar.layer.cornerRadius = 10;
+    navigationController.navigationBar.clipsToBounds = true;
+    navigationController.navigationBar.layer.maskedCorners = kCALayerMinXMinYCorner | kCALayerMaxXMinYCorner;
+
+    return navigationController;
+}
 
 @end
